c0dfd450dfc624d8b8a8b916c7310e0989cfd8a8
s-zhouj <s-zhouj@54L1809-21.it.bsd405.org>
2014-11-20 09:00:12 -0800

    yayayaya

--- FracCalc/src/Fraction.java
+++ FracCalc/src/Fraction.java
@@ -7,18 +7,22 @@ public class Fraction {
     private int denominator;
 
     public Fraction(int whole, int numerator, int denominator) {
+        //Error checking
         if (denominator <= 0) {
             throw new IllegalArgumentException("Denominator must be positive.");
         }
+        //setting
         this.denominator = denominator;
         this.numerator = whole * denominator + numerator;
         simplify();
     }
 
     public Fraction(int numerator, int denominator) {
+        //Error checking
         if (denominator <= 0) {
             throw new IllegalArgumentException("Denominator must be positive.");
         }
+        //setting
         this.denominator = denominator;
         this.numerator = numerator;
         simplify();
@@ -26,19 +30,23 @@ public class Fraction {
     }
 
     public Fraction(int whole) {
+        //I can't think of anything that would screw THIS up...
         this.numerator = whole;
         this.denominator = 1;
     }
 
     public Fraction(String s) {
+//reading the string
         s = s.replace("/", " ").replace("_", " ");
         Scanner sscan = new Scanner(s);
         int twhole = sscan.nextInt();
         int tnum = sscan.nextInt();
         int tden = sscan.nextInt();
+        //error checking
         if (tden <= 0) {
             throw new IllegalArgumentException("Denominator must be positive.");
         }
+        //setting
         this.denominator = tden;
         this.numerator = twhole * tden + tnum;
         simplify();
@@ -72,7 +80,8 @@ public class Fraction {
         this.numerator /= gcf(numerator, denominator);
     }
 
-    private static int lcm(int a, int b) {
+    private static int lcd(int a, int b) {
+        //Finds the lowest common denominator between A and B
         int result = a;
         while (result % b != 0) {
             result += a;
